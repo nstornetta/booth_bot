dbd4e1c8dfd9f16c9934c6396673614e9244fb1b
nstornetta <nstornetta@tumblr.com>
2017-07-14 13:12:53 -0400

    attempt to fix unpacking issue

--- run_bot.py
+++ run_bot.py
@@ -22,14 +22,14 @@ def parse_slack_output(slack_rtm_output):
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
-            if output and 'text' in output and AT_BOT in output['text']:
+            if output and 'text' in output and AT_BOT in output['text'] and 'user' in output:
                 # return text after the @ mention, whitespace removed
                 return (output['text'].split(AT_BOT)[1].strip().lower(),
                         output['channel'],
                         output['user']
                         )
 
-    return None, None
+    return None, None, None
 
 
 if __name__ == "__main__":
